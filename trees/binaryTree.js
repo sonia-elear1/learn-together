6504ae599bd889ae7e84fa29057aee13c81fcf4e
sonia-elear1 <soniadua7913@gmail.com>
2022-08-26 00:12:12 +0530

    binary tree, doubly linked list

--- trees/binaryTree.js
+++ trees/binaryTree.js
@@ -5,9 +5,18 @@ class newNode {
         this.right = null;
     }
 }
+
+class CreateNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null
+        this.previous = null
+    }
+}
 class BinaryTree {
     constructor() {
         this.root = null
+        this.head = null
     };
 
     /**
@@ -51,13 +60,13 @@ class BinaryTree {
             return;
 
         /* first recur on left child */
-        tree.printInorder(node.left);
+        this.printInorder(node.left);
 
         /* then print the data of node */
         console.log(node.data + " ");
 
         /* now recur on right child */
-        tree.printInorder(node.right);
+        this.printInorder(node.right);
     };
 
     /**
@@ -71,9 +80,9 @@ class BinaryTree {
             return;
 
         /* first recur on left child */
-        tree.printPostorder(node.left);
+        this.printPostorder(node.left);
         /* now recur on right child */
-        tree.printPostorder(node.right);
+        this.printPostorder(node.right);
         /* then print the data of node */
         console.log(node.data + " ");
     };
@@ -91,9 +100,9 @@ class BinaryTree {
         /* then print the data of node */
         console.log(node.data + " ");
         /* first recur on left child */
-        tree.printPreorder(node.left);
+        this.printPreorder(node.left);
         /* now recur on right child */
-        tree.printPreorder(node.right);
+        this.printPreorder(node.right);
 
     };
 
@@ -106,7 +115,7 @@ class BinaryTree {
         if (node == null)
             return 0;
         else
-            return Math.max(tree.height(node.left), tree.height(node.right)) + 1
+            return Math.max(this.height(node.left), this.height(node.right)) + 1
     }
 
     /**
@@ -140,10 +149,10 @@ class BinaryTree {
      */
     levelOrder = (node) => {
         let ans = [];
-        let height = tree.height(node);
+        let height = this.height(node);
         let i = 0
         while (i < height) {
-            ans.push(tree.printK(node, i));
+            ans.push(this.printK(node, i));
             i++
         }
         return ans
@@ -208,7 +217,7 @@ class BinaryTree {
             return 0
         }
         else {
-            return 1 + tree.sizeOfTree(node.left) + tree.sizeOfTree(node.right)
+            return 1 + this.sizeOfTree(node.left) + this.sizeOfTree(node.right)
         }
 
     }
@@ -228,11 +237,348 @@ class BinaryTree {
 
             // }
             // traverse(node)
-             
-            return Math.max(node.data, Math.max(tree.maxOfBinaryTree(node.left), tree.maxOfBinaryTree(node.right)))
+
+            return Math.max(node.data, Math.max(this.maxOfBinaryTree(node.left), this.maxOfBinaryTree(node.right)))
         }
         // return max
 
+    }
+
+    /**
+     * left view iterative method
+     * @param {*} node 
+     * @returns 
+     */
+    leftRightViewTree = (node) => {
+        let leftans = []
+        let rightans = []
+        let result = {
+            "leftView": [],
+            "rightView": []
+        }
+        let arr = [];
+        if (node === null) {
+            return
+        }
+        arr.push(node)
+        while (arr.length > 0) {
+            let count = arr.length;
+            for (let i = 0; i < count; i++) {
+                // get first element in queue
+                let temp = arr.shift();
+
+                // for right view
+                if (i == count - 1) {
+                    rightans.push(temp.data)
+                }
+                // for left view
+                if (i == 0) {
+                    leftans.push(temp.data)
+                }
+                if (temp.left != null) {
+                    arr.push(temp.left)
+                }
+                if (temp.right != null) {
+                    arr.push(temp.right)
+                }
+            }
+            result.leftView = result.leftView.concat([leftans])
+            result.rightView = result.rightView.concat([rightans])
+            leftans = []
+            rightans = []
+        }
+        return result
+    }
+
+    /**
+     * left view recursive
+     * @param {*} root 
+     * @returns 
+     */
+    leftView = (root) => {
+        //your code here
+        let maxLevel = 0;
+        let ans = []
+        function traverse(node, level) {
+            if (node === null) {
+                return
+            }
+            if (maxLevel < level) {
+                ans.push(node.data)
+                maxLevel = level
+            }
+            traverse(node.left, level + 1);
+            traverse(node.right, level + 1)
+        }
+        traverse(root, 1)
+        return ans
+    }
+
+    /**
+     * TOP AND BOTTOM VIEW OF BINARY TREE
+     * @param {*} node 
+     * @returns 
+     */
+    topBottomView = (node) => {
+        let arr = [];
+        let result = {
+            "topView": [],
+            "bottomView": []
+        }
+        let topmap = new Map()
+        let bottommap = new Map()
+        if (node === null) {
+            return
+        }
+        // hd is horizal distance from parent element
+        arr.push({ node: node, hd: 0 })
+        while (arr.length > 0) {
+            let count = arr.length;
+            for (let i = 0; i < count; i++) {
+                let temp = arr.shift();
+                // check if key not exist for top view
+                if (!topmap.has(temp.hd)) {
+                    topmap.set(temp.hd, temp.node.data)
+                }
+
+                // for bottom view
+                bottommap.set(temp.hd, temp.node.data)
+                // push elements in queue with horizontal distance
+                if (temp.node.left != null) {
+                    arr.push({ node: temp.node.left, hd: temp.hd - 1 })
+                }
+                if (temp.node.right != null) {
+                    arr.push({ node: temp.node.right, hd: temp.hd + 1 })
+                }
+            }
+        }
+
+        // get the value of keys of topview
+        topmap.forEach((data) => {
+            result.topView.push(data)
+        })
+        // get the value of keys of bottom view
+        bottommap.forEach((data) => {
+            result.bottomView.push(data)
+        })
+        return result
+    }
+
+    /**
+     * Validate if sum of chil is equal to parent in whole bt
+     * @param {*} node 
+     * @returns 
+     */
+    childSumProperty = (node) => {
+        let result = true
+        let sum = 0
+        function traverse(node) {
+            if (node === null) return true;
+            if (node.left === null && node.right === null) return true
+
+            if (node.left != null) {
+                sum += node.left.data
+            }
+
+            if (node.right != null) {
+                sum += node.right.data
+            }
+
+            console.log(node.data, node.left.data, node.right, "value")
+            if (node.data !== sum) {
+                result = false;
+                return result
+            }
+            sum = 0
+            traverse(node.left);
+            traverse(node.right);
+        }
+        traverse(node)
+        return result
+    }
+
+    /**
+     * check if tree if balanced for left and right side  means diffrence is not
+     * greater than 1
+     * Time Complexity  - O(N^2)
+     * @param {*} node 
+     * @returns 
+     */
+    balancedTree = (node) => {
+        if (node == null) { return true; }
+        else {
+            let left = this.height(node.left);
+            let right = this.height(node.right);
+            if (Math.abs(left - right) > 1) {
+                return false
+            }
+            this.balancedTree(node.left)
+            this.balancedTree(node.right)
+        }
+        return true
+    }
+
+    /** BALANCED TREE  
+     * TIME COMPLEXITY - O(N)
+     */
+    balancedTreeOptimize = (node) => {
+        if (node === null) return 0
+
+        let lh = this.balancedTreeOptimize(node.left)
+        if (lh === -1) return -1
+
+        let rh = this.balancedTreeOptimize(node.right)
+        if (rh === -1) return -1
+
+        if (Math.abs(lh - rh) > 1) {
+            return -1
+        }
+        else {
+            return Math.max(lh, rh) + 1
+        }
+    }
+
+    /**
+     * Maximum width of binary tree 
+     * implies maximum node at level 
+     * @param {*} node 
+     * @returns 
+     */
+    widthBT = (node) => {
+        let ans = 0
+        let max = -Infinity
+        let queue = []
+        queue.push(node)
+        while (queue.length > 0) {
+            let count = queue.length;
+            for (let i = 0; i < count; i++) {
+                let temp = queue.shift();
+                ans++;
+                if (temp.left !== null)
+                    queue.push(temp.left);
+                if (temp.right != null)
+                    queue.push(temp.right)
+            }
+            max = max > ans ? max : ans
+            ans = 0
+        }
+        return max
+    }
+
+    /**
+     * total nodes in binary tree
+     * @param {*} node 
+     */
+    totalNodes = (node) => {
+        let ans = 0;
+        let queue = []
+        queue.push(node);
+        while (queue.length > 0) {
+            let count = queue.length;
+            for (let i = 0; i < count; i++) {
+                let temp = queue.shift();
+                ans++;
+                if(temp.left!==null)
+                 queue.push(temp.left);
+                if(temp.right!==null)
+                 queue.push(temp.right)
+            }
+        }
+        return ans
+    }
+
+    /**
+     * convert binary tree into doubly linked list
+     * @param {*} node 
+     */
+    convertBTtoDLL = (node) => {
+        let prev = null
+        function traverse(node) {
+            if(node ===  null) return node;
+            /* first recur on left child */
+           let head = traverse(node.left);
+            /* then print the data of node */
+           if(prev === null) {
+             head = node
+           }
+           else {
+    
+             node.left = prev
+             prev.right = node
+           }
+           prev = node
+            /* now recur on right child */
+           traverse(node.right);
+        //    ans = head
+        return head
+        }
+        let ans = traverse(node);
+        return ans
+    }
+
+    createTreefromInPre = (ino, pre) => {
+        let is = 0;
+        let ie = ino.length - 1
+        let map = new Map()
+        for(let i = is;i<= ie; i++) {
+           map.set(ino[i], i)
+        }
+        let preIndex = 0;
+        let head;
+        function create(ino, pre, is, ie){
+            let inIndex;
+            if(is > ie) return null;
+            let root = new newNode(pre[preIndex]);
+            // console.log(root)
+            preIndex++;
+            // for(let i = is;i<= ie; i++) {
+            //     if(ino[i] === root.data){
+            //         inIndex = i;
+            //         break
+            //     }
+            // }
+            inIndex = map.get(root.data)
+            root.left = create(ino, pre, is, inIndex-1)
+            root.right = create(ino, pre, inIndex+1, ie)
+            return root;
+        }
+        head = create(ino, pre, is, ie)
+        return head
+    }
+
+
+    /**
+     * Create tree from inorder and postorder
+     * @param {*} ino 
+     * @param {*} post 
+     * @returns 
+     */
+    createTreefromInPost = (ino, post) => {
+        let is = 0;
+        let ie = ino.length - 1
+        let map = new Map()
+        for(let i = is;i<= ie; i++) {
+            map.set(ino[i], i)
+         }
+        let postIndex =  ino.length - 1;
+        let head;
+        function create(ino, post, is, ie){
+            let inIndex;
+            if(is > ie) return null;
+            console.log(post[postIndex])
+            let root = new newNode(post[postIndex]);
+            postIndex--;
+            inIndex = map.get(root.data)
+            // if (is == ie) {
+            //     return root;
+            //   }
+            root.right = create(ino, post, inIndex+1, ie)
+            root.left = create(ino, post, is, inIndex-1)
+            
+            return root;
+        }
+        head = create(ino, post, is, ie)
+        return head
     }
 }
 
@@ -245,11 +591,37 @@ tree.createBinaryTree(8);
 tree.createBinaryTree(7);
 tree.createBinaryTree(4);
 tree.createBinaryTree(11);
-tree.printInorder(tree.root);
-tree.printPostorder(tree.root);
-tree.printPreorder(tree.root);
-console.log(tree.height(tree.root))
-console.log(tree.printK(tree.root, 2))
-console.log(tree.levelOrderOptimize(tree.root))
-console.log(tree.sizeOfTree(tree.root), "size of bt")
-console.log(tree.maxOfBinaryTree(tree.root), "max of bt")
+console.log(JSON.stringify(tree.root))
+// tree.printInorder(tree.root);
+// tree.printPostorder(tree.root);
+// tree.printPreorder(tree.root);
+// console.log(tree.height(tree.root))
+// console.log(tree.printK(tree.root, 2))
+// console.log(tree.levelOrderOptimize(tree.root), "level order traversal")
+// console.log(tree.sizeOfTree(tree.root), "size of bt")
+// console.log(tree.maxOfBinaryTree(tree.root), "max of bt");
+// console.log(tree.leftRightViewTree(tree.root), "left and right view of bt")
+// console.log(tree.leftView(tree.root), "left view of bt using recursion")
+// console.log(tree.topBottomView(tree.root), "top View of bt")
+// console.log(tree1.childSumProperty(tree.root), "child sum property of bt")
+// console.log(tree.balancedTree(tree.root), "isBalanced 0.0")
+// console.log(tree.balancedTreeOptimize(tree.root), "isBalanced 0.1")
+// console.log(tree.widthBT(tree.root), "width of binary tree")
+// console.log(tree.totalNodes(tree.root), "total nodes in binary tree")
+// console.log(tree.convertBTtoDLL(tree.root), "convertBTtoDLL")
+
+let tree1 = new BinaryTree();
+tree1.createBinaryTree(5);
+tree1.createBinaryTree(10);
+tree1.createBinaryTree(3);
+tree1.createBinaryTree(1);
+tree1.createBinaryTree(2);
+tree1.createBinaryTree(4);
+
+// console.log(tree1.balancedTree(tree1.root), "isBalanced 1.0")
+// console.log(tree1.balancedTreeOptimize(tree1.root), "isBalanced 1.1")
+
+let tree2 = new BinaryTree();
+console.log(tree2.createTreefromInPre([ 'D', 'B', 'E' ,'A' ,'F' ,'C' ], ['A','B', 'D', 'E', 'C', 'F']), "createtree ")
+let tree3 = new BinaryTree();
+console.log(tree3.createTreefromInPost([ 4, 8, 2, 5, 1, 6, 3, 7], [8, 4, 5, 2, 6, 7, 3, 1]), "createtree postorder")
